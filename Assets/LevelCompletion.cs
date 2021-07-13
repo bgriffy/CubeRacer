48ee1129141ade608aa73d0fa790e52ad1e017f5
bgriffy <brentgriffin94@gmail.com>
2021-07-13 06:39:55 -0400

    Added level completion event - Added event load up next level once first level is completed - Added second level

---
+++ Assets/LevelCompletion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompletion : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        var nextSceneIndex = currentScene.buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex); 
+    }
+}
