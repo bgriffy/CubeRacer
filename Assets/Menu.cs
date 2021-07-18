81e4ad6d33da9f5916eaa8d83315ec9a7ea348aa
bgriffy <brentgriffin94@gmail.com>
2021-07-18 17:44:12 -0400

    Added start menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        var activeScene = SceneManager.GetActiveScene();
+        var activeSceneBuildIndex = activeScene.buildIndex;
+        SceneManager.LoadScene(activeSceneBuildIndex + 1); 
+    }
+}
